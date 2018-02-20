5419d7715e2af1ba9c4a6605f7d89d1f1a9bffd3
Keigo Yamamoto <k5.trismegistus@gmail.com>
2018-02-20 23:04:39 +0900

    feat: Rewrited grammer referincing JSX definition

--- src/visitor.py
+++ src/visitor.py
@@ -6,28 +6,26 @@ class Visitor():
         raise
 
     def component(self, ast, env):
-        visited = [self.visit(sub_ast, env) for sub_ast in ast.children]
-        return ''.join(visited)
+        return ''.join([self.visit(child_ast, env) for child_ast in ast.children])
 
-    def component_ref(self, ast, env):
+
+    def element(self, ast, env):
         key = self.visit(ast.children[0], env)
-        component = self.components[key]
-        return self.visit(component, env)
+        return self.visit(self.components[key], env)
 
-    def component_name(self, ast, env):
-        return ast.children[0].value
+    def identifier(self, ast, env):
+        return ''.join([child.value for child in ast.children])
 
-    def string(self, ast, env):
-        string = ''.join([child.value for child in ast.children])
-        return string
+    def text(self, ast, env):
+        return ''.join([child.value for child in ast.children])
 
     def visit(self, ast, env):
-        f = getattr(self, ast.data, self.__default__)
-
         try:
-            f(ast, env)
-        except:
+            f = getattr(self, ast.data)
+        except Exception as e:
             print(ast.data)
-            raise
+            raise e
+
+        f(ast, env)
 
         return f(ast, env)
